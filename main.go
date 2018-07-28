ed64cb05c182e72b57376d239059acbe3885930a
Shota HAMADA <maku3ts@gmail.com>
2018-07-28 13:48:55 +0900

    Write the tracking result to TT_HIST_FILE

--- main.go
+++ main.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"path"
+	"runtime"
 	"time"
 )
+
+func homeDir() string {
+	if runtime.GOOS == "windows" {
+		return os.Getenv("USERPROFILE")
+	}
+	return os.Getenv("HOME")
+}
 
 func main() {
 	t := time.Now()
 
+	tthist, ok := os.LookupEnv("TT_HIST_FILE")
+	if !ok {
+		tthist = path.Join(homeDir(), ".tthist")
+	}
+
 	if len(os.Args) != 2 {
-		fmt.Println("usage: tt taskname")
+		fmt.Print("usage: tt taskname\n\n")
 		os.Exit(1)
 	}
 
@@ -22,5 +36,18 @@ func main() {
 	signal.Notify(ch, os.Interrupt)
 	<-ch
 
-	fmt.Printf("tracking finished: %s: %v\n", taskname, time.Since(t).Round(time.Second))
+	elapsed := time.Since(t).Round(time.Second)
+
+	fmt.Printf("tracking finished: %s: %v\n", taskname, elapsed)
+
+	f, err := os.OpenFile(tthist, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = f.WriteString(fmt.Sprintf("\n%s\t%d", taskname, elapsed/time.Second))
+	if err != nil {
+		panic(err)
+	}
 }
